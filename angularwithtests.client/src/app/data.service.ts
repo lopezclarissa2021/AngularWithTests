e212cdb82d6a8752a228706b7a251623799c82f0
Jesse Harlan <jesse.harlan@centralia.edu>
2024-06-04 10:25:00 -0700

    removed dependency from data service in prep for lecture

--- angularwithtests.client/src/app/data.service.ts
+++ angularwithtests.client/src/app/data.service.ts
@@ -6,9 +6,5 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
-  constructor(private http: HttpClient) {
-    this.http.get<any[]>("http://localhost:5158/api/customers").subscribe(result => {
-      console.log(result);
-    })
-  }
+  constructor() { }
 }
